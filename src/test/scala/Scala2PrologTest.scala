cf532eeb52f94c0cf62a99a7da67b08b2c7cd49d
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-06-28 11:48:59 +0200

    add import Scala2Prolog to test method

--- src/test/scala/Scala2PrologTest.scala
+++ src/test/scala/Scala2PrologTest.scala
@@ -3,11 +3,14 @@ package pps.exam.application
 import alice.tuprolog.Term
 
 class Scala2PrologTest extends AbstractTest:
+  
+  import Scala2Prolog.newProxyInstanceOf
+  
   "Scala2Prolog" should :
     "reuse the same clauses in multiple calls of method's annotated with @PrologMethod from a common class annotated " +
       "with @PrologClass" in :
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestANonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")
       val prologResultB = proxy.methodB("X")
 
@@ -25,7 +28,7 @@ class Scala2PrologTest extends AbstractTest:
   "Scala2Prolog" should :
     "use only the clauses of the PrologClass if the PrologMethod's clauses are empty" in :
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestANonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultC = proxy.methodC("X")
 
       assert(prologResultC === Iterable(Term.createTerm("methodC(c)")))
@@ -33,7 +36,7 @@ class Scala2PrologTest extends AbstractTest:
   "Scala2Prolog" should :
     "not consider @PrologClass's clauses if empty, and parse only @PrologMethod's clauses instead" in :
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestAEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")
       val prologResultB = proxy.methodB("X")
 
@@ -50,7 +53,7 @@ class Scala2PrologTest extends AbstractTest:
     "return an empty Iterable of Term if no @PrologMethod nor @PrologClass clauses were defined, because the set" +
     "Theory in the tuProlog engine will be empty" in :
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestAEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultC = proxy.methodC("X")
 
       assert(prologResultC === Iterable.empty)
@@ -60,7 +63,7 @@ class Scala2PrologTest extends AbstractTest:
     "executing @PrologAddSharedClauses method's body" in :
       //@PrologClass clauses are 'Array("methodA(c).","methodB(c).")'
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestBNonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestB]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: methodA(c)
       proxy.clausesAdder()                                      // ?-     /       UPDATED THEORY: methodA(c), methodA(a)              METHOD BODY IS EMPTY
       val prologResultB = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: methodA(c), methodA(a)
@@ -79,7 +82,7 @@ class Scala2PrologTest extends AbstractTest:
       "invoking the method" in :
       //@PrologClass clauses are empty
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestBEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestB]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: *empty*
       proxy.clausesAdder()                                      // ?-     /       UPDATED THEORY: methodA(a)              METHOD BODY IS EMPTY
       val prologResultB = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: methodA(a)
@@ -98,7 +101,7 @@ class Scala2PrologTest extends AbstractTest:
       "annotated with @PrologClass" in :
       //@PrologClass annotation is not present (it's an optional annotation used only to define initial common clauses for each @PrologMethod)
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestBNotAnnotatedClassImpl().asInstanceOf[Scala2PrologDeclarationTestB]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: *empty*
       proxy.clausesAdder()                                      // ?-     /       UPDATED THEORY: methodA(a)              METHOD BODY IS EMPTY
       val prologResultB = proxy.methodA("X")                    // ?- methodA(X). CURRENT_THEORY: methodA(a)
@@ -115,5 +118,5 @@ class Scala2PrologTest extends AbstractTest:
   "Scala2Prolog" should :
     "throw an exception when invoking a method annotated with both @PrologMethod and @PrologAddSharedClauses" in :
       val scala2PrologDeclarationTest = Scala2PrologDeclarationTestBNonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestB]
-      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val proxy = newProxyInstanceOf(scala2PrologDeclarationTest)
       assertThrows[IllegalArgumentException](proxy.methodException("X"))
