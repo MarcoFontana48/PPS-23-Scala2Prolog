f39dc061b2b192d6e77f20f000edd151afc7c6d0
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-06-28 08:13:27 +0200

    add missing Scala2Prolog doc

--- src/main/scala/Scala2Prolog.scala
+++ src/main/scala/Scala2Prolog.scala
@@ -9,5 +9,13 @@ import org.apache.logging.log4j.scala.Logging
  * annotated Prolog logic execution.
  */
 object Scala2Prolog extends Logging:
+  /**
+   * Method to create a new proxy instance of the original object passed as argument.
+   * The proxy instance will handle the annotated Prolog logic.
+   *
+   * @param originalObject the original object to create a new proxy instance of.
+   * @tparam A the type of the original object.
+   * @return a new proxy instance of the original object.
+   */
   def newProxyInstanceOf[A](originalObject: A): A =
     PrologInterceptor.create(originalObject)
