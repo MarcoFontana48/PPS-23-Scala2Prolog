4ad9ac7abf913b7d032ed2cb01b33c1a948c0394
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-06-28 09:21:28 +0200

    add test empty @PrologMethod to use only @PrologClass clauses

--- src/test/scala/Scala2PrologTest.scala
+++ src/test/scala/Scala2PrologTest.scala
@@ -6,7 +6,7 @@ class Scala2PrologTest extends AbstractTest:
   "Scala2Prolog" should :
     "reuse the same clauses in multiple calls of method's annotated with @PrologMethod from a common class annotated " +
       "with @PrologClass" in :
-      val scala2PrologDeclarationTest = Scala2PrologDeclarationTestNonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTest]
+      val scala2PrologDeclarationTest = Scala2PrologDeclarationTestANonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
       val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")
       val prologResultB = proxy.methodB("X")
@@ -23,8 +23,16 @@ class Scala2PrologTest extends AbstractTest:
       )
 
   "Scala2Prolog" should :
+    "use only the clauses of the PrologClass if the PrologMethod's clauses are empty" in :
+      val scala2PrologDeclarationTest = Scala2PrologDeclarationTestANonEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
+      val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
+      val prologResultC = proxy.methodC("X")
+
+      assert(prologResultC === Iterable(Term.createTerm("methodC(c)")))
+
+  "Scala2Prolog" should :
     "not consider @PrologClass's clauses if empty, and parse only @PrologMethod's clauses instead" in :
-      val scala2PrologDeclarationTest = Scala2PrologDeclarationTestEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTest]
+      val scala2PrologDeclarationTest = Scala2PrologDeclarationTestAEmptyClausesImpl().asInstanceOf[Scala2PrologDeclarationTestA]
       val proxy = Scala2Prolog.newProxyInstanceOf(scala2PrologDeclarationTest)
       val prologResultA = proxy.methodA("X")
       val prologResultB = proxy.methodB("X")
